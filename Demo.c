7ac3c4255ee514b787acc8485dce34b92f64629b
Zirenow <81555658+Zirenow@users.noreply.github.com>
2021-07-16 18:58:47 +0200

    Changed function names

--- Demo.c
+++ Demo.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-	CL_Log("This is log function");
-	CL_Warning("This is warning function");
-	CL_Error("This is error function","(can also print details if you wish)");
+	EP_Log("This is log function");
+	EP_Warning("This is warning function");
+	EP_Error("This is error function","(can also print details if you wish)");
 }
